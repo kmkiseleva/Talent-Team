93507073d8fd02e25085baf48d2529d546a4537b
Ksenia Kiseleva <69579994+kmkiseleva@users.noreply.github.com>
2020-12-14 23:04:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function() {
        margin:10,
        nav:true,
        dots: false,
-       navText: ['<img src="../img/arrow_inactive.svg">','<img src="../img/arrow_active.svg">'],
+       navText: ['<img src="img/arrow_inactive.svg">','<img src="img/arrow_active.svg">'],
        responsive:{
            0:{
                items:1
@@ -24,7 +24,7 @@ $(function() {
       loop:true,
       margin:10,
       nav:true,
-      navText: ['<img src="../img/arrow_normal-l.svg">','<img src="../img/arrow_normal-r.svg">'],
+      navText: ['<img src="img/arrow_normal-l.svg">','<img src="img/arrow_normal-r.svg">'],
       responsive:{
           0:{
               items:1
