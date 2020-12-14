16696895b8a9fe631756dfd6fa00d9767aca677a
Ksenia Kiseleva <69579994+kmkiseleva@users.noreply.github.com>
2020-12-14 22:35:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 $(function() {
-   $('.owl-carousel').owlCarousel({
+    $('.reviews__slider').owlCarousel({
+       loop:true,
+       margin:10,
+       nav:true,
+       dots: false,
+       navText: ['<img src="../img/arrow_inactive.svg">','<img src="../img/arrow_active.svg">'],
+       responsive:{
+           0:{
+               items:1
+           },
+           600:{
+               items:2
+           },
+           1000:{
+               items:3
+           }
+       }
+   });
+ });
+
+$(function() {
+   $('.customers__slider').owlCarousel({
       loop:true,
       margin:10,
-      nav:false,
+      nav:true,
+      navText: ['<img src="../img/arrow_normal-l.svg">','<img src="../img/arrow_normal-r.svg">'],
       responsive:{
           0:{
               items:1
